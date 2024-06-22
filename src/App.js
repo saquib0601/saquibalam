4a6c1df21a3f1906dbbbed1123a2ab031f6572af
saquib0601 <saquibmallik007@gmail.com>
2024-06-22 12:43:18 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-// src/App.js
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import About from './components/About';
-import Contact from './components/Contact';
-import Experience from './components/Experience';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-black text-white">
-        <Header />
-        <main className="flex-grow mt-16">
-          <Routes>
-            <Route path="/" element={<Hero />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
